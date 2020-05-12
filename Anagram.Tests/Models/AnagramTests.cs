98b7d1fc7cebb1a9fd1de7a4412b36c85347edd5
unknown <macjay314@gmail.com>
2020-05-12 15:35:19 -0700

    Refactor first test to include a List<string>

--- Anagram.Tests/Models/AnagramTests.cs
+++ Anagram.Tests/Models/AnagramTests.cs
@@ -1,24 +1,44 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Anagram.Models;
 
 namespace Anagram.Tests{
   [TestClass]
-  public class AnagramTest{
+  public class AnagramTest : IDisposable{
+    public void Dispose(){
+      AnagramString.ClearAll();
+    }
+
     [TestMethod]
     public void AnagramConstructor_CreateInstanceOfAnagram_Anagram(){
-      AnagramString newAnagram = new AnagramString("test");
+      AnagramString newAnagram = new AnagramString("cat", "tac");
       Assert.AreEqual(typeof(AnagramString), newAnagram.GetType());
     }
 
     [TestMethod]
     public void GetWord_ReturnWord_string(){
+      string words = "beard";
       string word = "beard";
-      AnagramString anagram = new AnagramString(word);
+      AnagramString anagram = new AnagramString(words,word);
 
       string result = anagram.Word;
-
+      List<string> listResult = new List<string>{};
+      listResult = AnagramString.GetAll();
+      
       Assert.AreEqual(word, result);
+      Assert.AreEqual(words, listResult[0]);
     }
+
+    // [TestMethod]
+    // public void GetAll_ReturnsEmptyList_AnagramList()
+    // {
+    //   AnagramString newList = new AnagramString();
+
+    //   List<AnagramString> result = newList.GetAll();
+
+    //   CollectionAssert.AreEqual(newList, result);
+    // }
   }
   
 }
