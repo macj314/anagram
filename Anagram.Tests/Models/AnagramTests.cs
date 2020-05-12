d5b37c3ffa7570f3eb959eeafaca1e7c920bb60f
unknown <macjay314@gmail.com>
2020-05-12 14:36:30 -0700

    Create test for returning Word propert of Anagram object

--- Anagram.Tests/Models/AnagramTests.cs
+++ Anagram.Tests/Models/AnagramTests.cs
@@ -6,10 +6,19 @@ namespace Anagram.Tests{
   public class AnagramTest{
     [TestMethod]
     public void AnagramConstructor_CreateInstanceOfAnagram_Anagram(){
-      AnagramString newAnagram = new AnagramString();
+      AnagramString newAnagram = new AnagramString("test");
       Assert.AreEqual(typeof(AnagramString), newAnagram.GetType());
     }
 
+    [TestMethod]
+    public void GetWord_ReturnWord_string(){
+      string word = "beard";
+      AnagramString anagram = new AnagramString(word);
+
+      string result = anagram.Word;
+
+      Assert.AreEqual(word, result);
+    }
   }
   
 }
